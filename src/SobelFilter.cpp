639e811bfb103a9a72527ac2ab8de8fb505a7aa7
frapama6 <izgngr@gmail.com>
2025-07-04 19:42:34 +0200

    Steps + lena.jpg

--- src/SobelFilter.cpp
+++ src/SobelFilter.cpp
@@ -5,27 +5,92 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 
+using namespace cv;
+using namespace std;
+
 
 int main(int argc, char** argv)
 {
-    if (argc != 2)
+
+    cv::CommandLineParser parser(argc, argv,
+        "{@input   |lena.jpg|input image}"
+        "{ksize   k|1|ksize (hit 'K' to increase its value at run time)}"
+        "{scale   s|1|scale (hit 'S' to increase its value at run time)}"
+        "{delta   d|0|delta (hit 'D' to increase its value at run time)}"
+        "{help    h|false|show help message}");
+
+    cout << "The sample uses Sobel or Scharr OpenCV functions for edge detection\n\n";
+    parser.printMessage();
+    cout << "\nPress 'ESC' to exit program.\nPress 'R' to reset values ( ksize will be -1 equal to Scharr function )";
+
+    // First we declare the variables we are going to use
+    Mat image, src, src_gray;
+    Mat grad;
+    const String window_name = "Sobel Demo - Simple Edge Detector";
+    int ksize = parser.get<int>("ksize");
+    int scale = parser.get<int>("scale");
+    int delta = parser.get<int>("delta");
+    int ddepth = CV_16S;
+
+    String imageName = parser.get<String>("@input");
+    // As usual we load our source image (src)
+    image = imread(samples::findFile(imageName), IMREAD_COLOR); // Load an image
+
+    image.datastart;
+
+    // Check if image is loaded fine
+    if (image.empty())
     {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
+        printf("Error opening image: %s\n", imageName.c_str());
+        return EXIT_FAILURE;
     }
 
-    cv::Mat image;
-    image = cv::imread(argv[1], cv::IMREAD_COLOR);
-
-    if (!image.data)
+    for (;;)
     {
-        printf("No image data \n");
-        return -1;
+        // Remove noise by blurring with a Gaussian filter ( kernel size = 3 )
+        GaussianBlur(image, src, Size(3, 3), 0, 0, BORDER_DEFAULT);
+
+        // Convert the image to grayscale
+        cvtColor(src, src_gray, COLOR_BGR2GRAY);
+
+        Mat grad_x, grad_y;
+        Mat abs_grad_x, abs_grad_y;
+
+        Sobel(src_gray, grad_x, ddepth, 1, 0, ksize, scale, delta, BORDER_DEFAULT);
+
+        Sobel(src_gray, grad_y, ddepth, 0, 1, ksize, scale, delta, BORDER_DEFAULT);
+
+        // converting back to CV_8U
+        convertScaleAbs(grad_x, abs_grad_x);
+        convertScaleAbs(grad_y, abs_grad_y);
+
+        addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+
+        char key = (char)waitKey(0);
+
+        if (key == 27)
+        {
+            return EXIT_SUCCESS;
+        }
+
+        if (key == 'k' || key == 'K')
+        {
+            grad = abs_grad_x;
+        }
+
+        if (key == 's' || key == 'S')
+        {
+            grad = abs_grad_y;
+        }
+
+        if (key == 'd' || key == 'D')
+        {
+            addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+        }
+
+        imshow(window_name, grad);
+
     }
-    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Display Image", image);
-
-    cv::waitKey(0);
-
-    return 0;
+    return EXIT_SUCCESS;
 }
+
--- src/SobelFilter.h
+++ src/SobelFilter.h
@@ -1,9 +1,14 @@
-﻿// SobelFilter.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
+﻿#pragma once
 
 #include <iostream>
 
+/*
+-Denoiser (Gaussian)
+-Convert to greyscale (OpenCV usa BGR)
+-Sobel vertical
+-Sobel horizontal
+-Rescale (Convert back to unsigned 8bit integer)
+-Add both sobel weighted 50% each
+-save it (and/or show it)
+*/
 
-// TODO: Reference additional headers your program requires here.
